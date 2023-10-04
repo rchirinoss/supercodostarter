a909fbff38b495ee997641f4358c32aa6bc702a6
RomySchab <rominaschab@hotmail.com>
2023-10-04 00:03:31 -0300

    animacion logo, volver y casi me gusta

---
+++ js/like.js
@@ -0,0 +1,13 @@
+const likeBtn = document.querySelector(".like_btn");
+let likeIcon=document.querySelector("#icon");
+let count = document.querySelector("#count");
+let clicked=false;
+likeBtn.addEventListener("click",()=>{
+    if(!clicked){
+        clicked=true;
+        likeIcon.innerHTML=`<i class="fas fa-thumbs-up"></i>`;
+        count.textContent++;
+    }else{
+        clicked=false;
+        }
+   })
